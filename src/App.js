beafdb4fe16fcfb82ad7ebb4d21cca095f186de2
OsmandLN <osmand8370@gmail.com>
2022-10-31 10:07:22 +0800

    feat: add item feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+
+// App 父元件
+const App = () => {
+  const [list, setList] = useState([])
+
+  const onAddClicked = (message) => {
+    let newList = [...list, message]
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddPanel onAddClicked={onAddClicked} />
+      {/*前後重複是必然的嗎? 還是只是偶然呢?*/}
+      <TodoList list={list} />
+    </>
+
+
   );
 }
+export default App
 
-export default App;
+// AddPanel 子元件
+const AddPanel = ({ onAddClicked }) => {
+  {/*從父層傳入onAddClicked方法供子元件使用 => props*/ }
+  const [message, setMessage] = useState('')
+
+  //建立輸入框輸入事件的處理
+  const onMessageInput = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const addTodo = (event) => {
+    onAddClicked(message)
+    setMessage('') /*內容清空*/
+  }
+  return (
+    <>
+      <input value={message} onChange={onMessageInput} />
+      {/*onChange 為內建事件 */}
+      <button onClick={addTodo}>Add</button>
+    </>
+  )
+}
+
+// TodoList 子元件
+const TodoList = ({ list }) => (
+  /*調用props傳過來的list(陣列)*/
+  < ul >
+    {
+      list.map((todoItem) => {
+        return (
+          <>
+            <li>{todoItem} <button>X</button></li >
+          </>
+        )
+      })
+    }
+  </ul >
+)
+console.log(TodoList)
